b997639ca0ba0030f327077b05571c6c6b4d4fdf
galAntonis <galant.dev@gmail.com>
2023-10-28 21:15:35 +0300

    header design implementation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 
-import {Navbar} from './components'
+import {Navbar,Header} from './components'
 
 import './App.css'
 
@@ -9,7 +9,8 @@ const App = () => {
   return (
 	<div className="App">
 		<div className='gradient__bg'>
-			<Navbar/>
+      <Header/>
+      <Navbar/>
 		</div>
 	</div>
   )
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './header.css'
+
+import userlogo from '../../assets/user-logo.png';
+import logout from '../../assets/logout.png';
+import notification from '../../assets/notification.png';
+
+const Header = () =>{
+	return (
+		<div className='sma_header'>
+			<div className="sma_header-title">
+				<p>Overview Page</p>
+			</div>
+			<div className='sma_header-links'>
+				<img src={notification} width='40px' height='40px'/>
+				<img src={userlogo} width='40px' height='40px'/>
+				<img src={logout} width='40px' height='40px'/>
+			</div>
+		</div>
+	)
+}
+
+export default Header
