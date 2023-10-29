0f89e5f8d8e202eba99ddfab319d6c3c88113b88
galAntonis <galant.dev@gmail.com>
2023-10-29 12:32:17 +0200

    Try to add the grid panels

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 
 
-import {Navbar,Header} from './components'
+import {Navbar,Header, Overview} from './components'
 
 import './App.css'
 
 const App = () => {
   return (
 	<div className="App">
-		<div className='gradient__bg'>
-      <Header/>
-      <Navbar/>
-		</div>
+			<Header/>
+			<Navbar/>
+			<Overview/>
 	</div>
   )
 }
---
+++ src/components/overview/Overview.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './overview.css'
+
+const Overview = () =>{
+
+	return (
+		<div class="sma_overview">
+			<div className="sma_overview-hello">
+				<div className="sma_overview-hello_title">
+					Hello
+				</div>
+				
+			</div>
+			<div className="sma_overview-grid">
+				<div id="item-0">
+					
+				</div>
+				<div id="item-1">&nbsp;</div>
+				<div id="item-2">&nbsp;</div>
+				<div id="item-3">&nbsp;</div>
+				<div id="item-4">&nbsp;</div>
+				<div id="item-5">&nbsp;</div>
+				<div id="item-6">&nbsp;</div>
+			</div>
+			
+		</div>
+		
+	)
+}
+
+export default Overview
