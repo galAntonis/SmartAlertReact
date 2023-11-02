acd9d81d683a198167e55d99aee8d0f9b28f7c93
galAntonis <galant.dev@gmail.com>
2023-11-02 22:44:17 +0200

    Home panels design

--- src/components/panel/Panel.jsx
+++ src/components/panel/Panel.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
 import './panel.css'
-const Panel = () =>{
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	FaEllipsisH
+}from "react-icons/fa";
+const Panel = ({imgUrl, title, subTitle, subTitle2}) =>{
 
 	return (
-		<div>
-			<p>panel</p>			
+		<div className='sma_home-panel'>
+			<div className="sma_home-panel_content">
+				<div className="sma_home-panel_image">
+					<FontAwesomeIcon icon={imgUrl} />
+					
+				</div>
+				<div className="sma_home-panel_image-text">
+						<div className="sma_home-panel_image-primtext">
+						{title}
+						</div>	
+						<div className="sma_home-panel_image-sectext_container">
+							<div className="sma_home-panel_image-sectext">
+							{subTitle}
+							</div>
+							<div className="sma_home-panel_image-sectext2">
+							{subTitle2}
+							</div>
+						</div>
+				</div>
+			</div>
+			
+			<div className="sma_home-panel_more">
+				<FaEllipsisH/>
+			</div>
+			<div className="sma_home-panel_content">
+				<h3></h3>
+				<h5></h5>
+			</div>			
 		</div>
 		
 	)
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,20 +1,27 @@
 import React from "react";
 import './home.css'
-
+import { Panel } from "../../components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faUserPlus, // the clock icon
+	faHandHoldingMedical, // the user circle icon
+	faCircleRadiation,
+	faCirclePlus // a cup of coffee
+} from "@fortawesome/free-solid-svg-icons";
 
 const Home = () => {
 	return (
 		<div class="sma_home">
 			<div className="sma_home-grid selected_row">
 
-				<div id="item-0">
+				<div id="item-0"><Panel imgUrl={faCirclePlus} title='52'	subTitle='New Incidents'/>
 				</div>
-				<div id="item-1">&nbsp;</div>
+				<div id="item-1"><Panel imgUrl={faCircleRadiation} title='142'	subTitle='Total Incidents'/></div>
 				<div id="item-2">&nbsp;</div>
-				<div id="item-3">&nbsp;</div>
-				<div id="item-4">&nbsp;</div>
+				<div id="item-3"><Panel imgUrl={faHandHoldingMedical} title='2.653' subTitle='Lives saved'/></div>
+				<div id="item-4"><Panel imgUrl={faUserPlus} title='23'	subTitle='New users' subTitle2='This month'/></div>
 
-				<div id="item-5">&nbsp;</div>
+				<div id="item-5"></div>
 				<div id="item-6">&nbsp;</div>
 			</div>
 		</div>
