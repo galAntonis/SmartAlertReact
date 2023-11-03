3ad1979394307f347f0d37adf3b83ee195b06812
galAntonis <galant.dev@gmail.com>
2023-11-03 19:05:44 +0200

    Title change when page changes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter, Routes , Route} from 'react-router-dom';
+import { BrowserRouter,Router, Routes , Route, useLocation } from 'react-router-dom';
 
 import {Navbar,Header, Overview} from './components'
 import {Home,Statistics,Alerts,Settings,Help} from './pages';
 
 import './App.css'
 
+function Title(){
+	const location = useLocation();
+	return(
+		<Header route={location.pathname} />
+	);
+	
+}
 const App = () => {
+	
 	return (
 		<BrowserRouter>
-			<Header/>
+			<Title/>
 			<Navbar>
 				<Routes>
-					<Route path="/" element={<Home name='Antonis'/>}/>
-					<Route path="/index" element={<Home name='Antonis'/>}/>
-					<Route path="/statistics" element={<Statistics/>}/>
-					<Route path="/alerts" element={<Alerts/>}/>
-					<Route path="/settings" element={<Settings/>}/>
-					<Route path="/help" element={<Help/>}/>
+				<Route path="/" element={<Home name='Antonis' />} />
+				<Route path="/index" element={<Home name='Antonis' />} />
+				<Route path="/statistics" element={<Statistics />} />
+				<Route path="/alerts" element={<Alerts />} />
+				<Route path="/settings" element={<Settings />} />
+				<Route path="/help" element={<Help />} />
 				</Routes>
 			</Navbar>
-			
-			  
-		</BrowserRouter>
+   		</BrowserRouter>
 	);
   };
   
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,13 +6,35 @@ import {
 }from "react-icons/fa";
 
 import { Tooltip } from "react-tooltip";
-const Header = () =>{
+const Header = ({ route }) =>{
+	let headerText = '';
+
+	switch (route) {
+	  case '/':
+	  case '/index':
+		headerText = 'Overview';
+		break;
+	  case '/statistics':
+		headerText = 'Statistics';
+		break;
+	  case '/alerts':
+		headerText = 'Alerts';
+		break;
+	  case '/settings':
+		headerText = 'Settings';
+		break;
+	  case '/help':
+		headerText = 'Help';
+		break;
+	  default:
+		headerText = 'Page Not Found';
+	}
 	return (
 		<div className='sma_header'>
 			<Tooltip id="top-icons_tooltip" place="bottom" effect='float'/>
 			<div></div>
 			<div className="sma_header-title">
-				<p>Overview Page</p>
+				<p>{headerText}</p>
 			</div>
 			<div className='sma_header-links'>
 				<a data-tooltip-id="top-icons_tooltip" data-tooltip-content='Notifications'><FaBell/></a>
